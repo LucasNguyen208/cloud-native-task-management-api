09a2f34adbfbe20edd7a7e7823b248ae1fa57180
Long Nguyen <lucaswnguyen@gmail.com>
2026-05-11 13:42:12 +0700

    Add Task model and migration for tasks table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.role import Role as Role
 from app.models.user import User as User
+from app.models.task import Task as Task
 
-__all__ = ["Role", "User"]
+__all__ = ["Role", "User", "Task"]
